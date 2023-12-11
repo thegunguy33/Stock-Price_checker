4f4f34ae3ada5e156b0e7914b5fa3eac7fe15d2d
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-11 01:19:25 -0500

    depreciated db

--- db-connection.js
+++ db-connection.js
@@ -1,35 +1,36 @@
 const mongoose = require('mongoose');
 
 // Your MongoDB connection string
-const dbURI = process.env.MONGODB_URI || `mongodb+srv://jaredbennett33:W4rz0n3@cluster0.ibs8sxe.mongodb.net/?retryWrites=true&w=majority
-`;
+const uri = 'mongodb+srv://jaredbennett33:W4rz0n3@cluster0.ibs8sxe.mongodb.net/test?retryWrites=true&w=majority';
 
-// Connect to MongoDB
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
 });
 
 // Event listeners for connection status
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose connected to ' + dbURI);
+connection.on('connected', () => {
+  console.log('Mongoose connected to ' + uri);
 });
 
-mongoose.connection.on('error', (err) => {
+connection.on('error', (err) => {
   console.log('Mongoose connection error: ' + err);
 });
 
-mongoose.connection.on('disconnected', () => {
+connection.on('disconnected', () => {
   console.log('Mongoose disconnected');
 });
 
 // Gracefully close the connection on process termination
 process.on('SIGINT', () => {
-  mongoose.connection.close(() => {
+  connection.close(() => {
     console.log('Mongoose disconnected through app termination');
     process.exit(0);
   });
 });
 
 // Export the connection
-module.exports = mongoose.connection;
+module.exports = connection;

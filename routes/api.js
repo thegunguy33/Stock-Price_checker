c81726775e42b024c285a854c98fff972954b908
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-11 00:25:57 -0500

    Almost Done?

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,100 @@ const router = express.Router();
 const StockController = require('../controllers/stockController');
 
 // Routes for the Stock API
-router.route('/stock-prices')
-  .get(StockController.getStockPrices);
+//router.route('/stock-prices')
+//  .get(StockController.getStockPrices);
+
+async function createStock(stock, like, ip) {
+  const newStock = new StockModel({
+    symbol: stock,
+    likes: like ? [ip] : [],
+  });
+  const savedNew = await newStock.save();
+  return savedNew;
+}
+
+async function findStock(stock) {
+  return await StockModel.findOne({ symbol: stock }).exec();
+}
+
+
+
+async function saveStock(stock, like, ip) {
+  let saved = {};
+  const foundStock = await findstock(stock);
+  if (!foundStock) {
+    const createsaved = createStock(stock, like, ip);
+    saved = createsaved;
+    return saved;
+    } else {
+      if (like && foundStock.likes.indexOf(ip) === -1) {
+        foundStock.likes.push(ip);
+      }
+      saved = await foundStock.save();
+      return saved;
+    } 
+  }
+
+
+
+async function getStock(stock) {
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock${stock}/quote`
+  );
+
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice };
+
+}
+
+
+module.exports = function (app) {
+  app.route("/api/stock-prices").get(async function (req, res) {
+    const { stock, like } = req.query;
+    if (Array.isArray(stock)) {
+      console.log("stocks", stock);
+
+      const { symbol: symbol1, latestPrice: latestPrice1 } = await getStock(stock[0]);
+      const { symbol: symbol2, latestPrice: latestPrice2 } = await getStock(stock[1]);
+
+      const firststock = await saveStock(stock[0], like, req.ip);
+      const secondstock = await saveStock(stock[1], like, req.ip);
+
+      let stockData = [];
+      if (!symbol1) {
+        stockData.push({
+          rel_likes: firststock.likes.length - secondstock.likes.length,
+        });
+      } else {
+        stockData.push({
+          stock: symbol1,
+          price: latestPrice1,
+          rel_likes: firststock.likes.length - secondstock.likes.length,
+        });
+      }
+
+      if (!symbol2) {
+        stockData.push({
+          stock: symbol2,
+          price: latestPrice2,
+          rel_likes: secondstock.likes.length - firststock.likes.length,
+        });
+
+        res.json({
+          stockData,
+        });
+        return;
+      }
+    }
+
+    const { symbol: symbol3, latestPrice: latestPrice3 } = await getStock(stock);
+    if (!symbol3) {
+      res.json({ stockData: { likes: like ? 1 : 0 } });
+      return;
+    }
+  });
+};
 
 //https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/[symbol]/quote
 
-module.exports = router;
+//module.exports = router;
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,58 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const assert = chai.assert;
-const app = require('../server'); // Assumes your server file is in the root directory
+const app = require('../src/server'); // Make sure to adjust the path based on your actual file structure
 
 chai.use(chaiHttp);
+const { expect } = chai;
 
 describe('Functional Tests', () => {
-  // Write your functional tests here
+  it('Viewing one stock: GET request to /api/stock-prices/', (done) => {
+    chai.request(app)
+      .get('/api/stock-prices/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        // Add more assertions as needed
+        done();
+      });
+  });
+
+  it('Viewing one stock and liking it: GET request to /api/stock-prices/', (done) => {
+    chai.request(app)
+      .get('/api/stock-prices/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        // Add more assertions as needed, including liking the stock
+        done();
+      });
+  });
+
+  it('Viewing the same stock and liking it again: GET request to /api/stock-prices/', (done) => {
+    chai.request(app)
+      .get('/api/stock-prices/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        // Add more assertions as needed, including liking the stock again
+        done();
+      });
+  });
+
+  it('Viewing two stocks: GET request to /api/stock-prices/', (done) => {
+    chai.request(app)
+      .get('/api/stock-prices/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        // Add more assertions as needed, including checking two stocks
+        done();
+      });
+  });
+
+  it('Viewing two stocks and liking them: GET request to /api/stock-prices/', (done) => {
+    chai.request(app)
+      .get('/api/stock-prices/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        // Add more assertions as needed, including liking both stocks
+        done();
+      });
+  });
 });

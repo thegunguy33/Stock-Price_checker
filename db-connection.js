977e3f26a78009c48b0396526c35e259aeaa3b36
thegunguy33 <78658443+thegunguy33@users.noreply.github.com>
2023-12-10 22:48:15 -0500

    Corrected DB issues

--- db-connection.js
+++ db-connection.js
@@ -1,7 +1,34 @@
-const mongoose = require("mongoose");
-const db = mongoose.connect(process.env.DB, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+const mongoose = require('mongoose');
+
+// Your MongoDB connection string
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://jaredbennett33:W4rz0n3@cluster0.ibs8sxe.mongodb.net/?retryWrites=true&w=majority';
+
+// Connect to MongoDB
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-module.exports = db;
+// Event listeners for connection status
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+// Gracefully close the connection on process termination
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+// Export the connection
+module.exports = mongoose.connection;
